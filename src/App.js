237efe253b56a4d831ca5d2947d3fd6a76f1a7ba
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-26 06:40:45 +0530

    created first draft of landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav>
+          <ul>
+            <li>logo</li>
+            <li>login</li>
+            <li>contact</li>
+            <li>apply</li>
+          </ul>
+        </nav>
+        <div>
+          <h1>LinkWork</h1>
+          <h3>Earn Minimum Rs. 50,000</h3>
+          <h3>Without spending any time or money</h3>
+        </div>
+        <div>
+          <h2>Refer a client and become a link. arn 20% of consideration without any limit in first year. After that upto Rs. 50,000</h2>
+          <p>WHAT IS LINKWORK: LinkWork is worlds first and only platform for which let's you make money without doing any work or spending any time.</p>
+          <p>HOW CAN I MAKE MONEY: by referring clients and creating a chain of people working under you.</p>
+          <p>HOW CAN I CREATE A CHAIN: After you are accepted, you can add more people under you and create a chain. For more details on creating chain go to this LINK.</p>
+          <p>HOW MANY HOURS DO I NEED TO WORK: Zero. You don't have to work at all. But you can work if you want. Just refer a client to us and we will take care of rest.</p>
+          <p>HOW WILL LINKWORK PROVIDE SERVICES TO CLIENT: LinkWork is connected with world class professional firms. Our network of professional firms will provide the services to client.</p>
+          <p>WHAT TYPE OF CLIENTS: GST, Accounts, Starting Business, Company Registration, Income Tax, Labour Law or any other legal or financial service.</p>
+          <p>DO I NEED TO PAY ANYTHING: NO. We will never ask you to pay anything. Never. Ever.</p>
+          <p>I HAVE MORE QUESTIONS: You can apply and provide your details. I we find your profile suitable we will call you and you can ask all the questions that you have.</p>
+          <p>HOW MUCH MONEY CAN I MAKE: If you work alone you will make minimum 50,000. If you create your chain of people, there's no limit on the amounnt of money you can make. Maximum depends on your performance.</p>
+          <p>HOW CAN I PASS THE INTERVIEW: If you have links and work with businessmen, there's a good chance you will be accepted. If you have experience of dealing with people. If your job includes dealing with clients. If you have worked in B2B industry.</p>
+          <br/><br/><br/>
+        </div>
+        <section>
+          <h3>Here's how you can do it</h3>
+          <p>Step 1: Clear interview and get accepted</p>
+          <p>Apply for an interview, do the interview and if you are accepted you can start right away.</p>
+          <p>Step 2: Give referrals</p>
+          <p>Once you are accepted, all you have to do is just keep you eyes and ears open. Whenever you find someone who needs legal and financial services, you just tell them that you know us and we can provide them the services they need.</p>
+          <p>Step 3: get incentives in your bank</p>
+          <p>We will contact the client. We will meet the client and if client agrees to give us the work we will do the work and raise the bill. Once client pays the bill, you will get your incentive.</p>
+          <p>Step 4: Create your own chain (actually you can start creating your chain whenever you want. We suggest asap)</p>
+          <p>If you know poeple who have links with bussinesses and firms, you can add them in your chain. They will do the same work. Refer clients to us. We will pay incentive to them as well as you. Building your chain is a great way of making a lot of money. larger the chain, more the incentive.</p>
+        </section>
+        <section>
+          <h2>How can I create a chain?</h2>
+          <p>Add more links below yourself Make 10% of the consideration from link level 1 Make 5% of the consideration from link level 2</p>
+        </section>
+        <section>
+          <p>We are a completely remote company. You don't have to come to office. You can work from wherever you want.</p>
+        </section>
+        <footer>
+          <p>contact</p>
+          <p>apply</p>
+        </footer>
     </div>
   );
 }
