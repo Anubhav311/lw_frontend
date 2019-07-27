b80f738456aa2ce9c25b3878b9ae305bbee54e82
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-27 06:01:48 +0530

    added header image

---
+++ src/faq/Faqs.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+
+
+let faqContent = {
+    LinkWork: {
+      questionOne: {
+        title: "WHAT IS LINKWORK?",
+        content: "LinkWork is worlds first and only platform for which let's you make money without doing any work or spending any time.",
+      },
+      questionTwo: {
+        title: "HOW WILL LINKWORK PROVIDE SERVICES TO CLIENT?",
+        content: "LinkWork is connected with world class professional firms. Our network of professional firms will provide the services to client.",
+      },
+      questionThree: {
+        title: "WHAT TYPE OF CLIENTS?",
+        content: "GST, Accounts, Starting Business, Company Registration, Income Tax, Labour Law or any other legal or financial service.",
+      }
+    },
+    HowMakeMoney : {
+      questionOne: {
+        title: "HOW CAN I MAKE MONEY",
+        content: "by referring clients and creating a chain of people working under you.",
+      },
+      questionTwo: {
+        title: "HOW MANY HOURS DO I NEED TO WORK?",
+        content: "Zero. You don't have to work at all. But you can work if you want. Just refer a client to us and we will take care of rest.",
+      },
+      questionThree: {
+        title: "DO I NEED TO PAY ANYTHING?",
+        content: "NO. We will never ask you to pay anything. Never. Ever.",
+      }
+    },
+    CreateChain : {
+      questionOne: {
+        title: "HOW CAN I CREATE A CHAIN?",
+        content: "After you are accepted, you can add more people under you and create a chain. For more details on creating chain go to this LINK.",
+      },
+      questionTwo: {
+        title: "WHAT IS THE BENEFIT OF CREATING A CHAIN?",
+        content: "You chain will give Business to LinkWork and we will pay incentive to you and your chain.",
+      },
+      questionThree: {
+        title: "HOW MANY PEOPLE CAN I ADD IN MY CHAIN?",
+        content: "you can add 5 people. Then those 5 people can further add 5 people each. This way you will have 25 people in your chain.",
+      },
+      questionFour: {
+        title: "HOW MUCH INCENTIVE WILL I GET FROM MY CHAIN",
+        content: "You will get 20% commission from a direct client. You will earn 10% from people on level 1 of your chain. You will earch 5% from people on level 2 of your chain."
+      }
+    }
+  
+  }
+
+  
+export default faqContent;
