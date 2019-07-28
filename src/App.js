14f158f92d3d34808454cad5757d2bc99df660f7
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-28 23:28:13 +0530

    added more illustrations

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+
 import Accordion from "./faq/accordion";
 import Tabs from "./faq/Tabs";
-
 import './App.css';
 import faqContent from './faq/Faqs';
-import header_image from './assets/header_image.png';
+import header_img from './assets/header_img.png';
+import chain_img from './assets/chain_img.png';
+import incentive_img from './assets/incentive_img.png';
+import interview_img from './assets/interview_img.png';
+import refer_img from './assets/refer_img.png';
 
 
 function App() {
   console.log(faqContent.LinkWork)
   return (
     <div className="App">
+
         <nav className="navbar">
           <p>LinkWork</p>
           <ul className="landing-page-navbuttons">
@@ -20,6 +25,7 @@ function App() {
             <li>Login</li>
           </ul>
         </nav>
+
         <div className="header">
           <div>
             <h1>LinkWork</h1>
@@ -27,14 +33,14 @@ function App() {
             <p>Without spending any time or money</p>
             <button>Apply</button>
           </div>
-          <img style={{width: "60%"}} src={header_image} alt="header image"/>
+          <img style={{width: "60%"}} src={header_img} alt="header image"/>
         </div>
+
         <div>
           <h2>Refer a client and become a link. arn 20% of consideration without any limit in first year. After that upto Rs. 50,000</h2>
           <p>I HAVE MORE QUESTIONS: You can apply and provide your details. I we find your profile suitable we will call you and you can ask all the questions that you have.</p>
           <p>HOW MUCH MONEY CAN I MAKE: If you work alone you will make minimum 50,000. If you create your chain of people, there's no limit on the amounnt of money you can make. Maximum depends on your performance.</p>
           <p>HOW CAN I PASS THE INTERVIEW: If you have links and work with businessmen, there's a good chance you will be accepted. If you have experience of dealing with people. If your job includes dealing with clients. If you have worked in B2B industry.</p>
-          <br/><br/><br/>
         </div>
         <Tabs className="tabs">
           <div label="What is LinkWork">
@@ -89,12 +95,16 @@ function App() {
           <h3>Here's how you can do it</h3>
           <p>Step 1: Clear interview and get accepted</p>
           <p>Apply for an interview, do the interview and if you are accepted you can start right away.</p>
+          <img style={{width: "60%"}} src={interview_img} alt="interview image"/>
           <p>Step 2: Give referrals</p>
           <p>Once you are accepted, all you have to do is just keep you eyes and ears open. Whenever you find someone who needs legal and financial services, you just tell them that you know us and we can provide them the services they need.</p>
+          <img style={{width: "60%"}} src={refer_img} alt="refer image"/>
           <p>Step 3: get incentives in your bank</p>
           <p>We will contact the client. We will meet the client and if client agrees to give us the work we will do the work and raise the bill. Once client pays the bill, you will get your incentive.</p>
+          <img style={{width: "60%"}} src={incentive_img} alt="incentive image"/>
           <p>Step 4: Create your own chain (actually you can start creating your chain whenever you want. We suggest asap)</p>
           <p>If you know poeple who have links with bussinesses and firms, you can add them in your chain. They will do the same work. Refer clients to us. We will pay incentive to them as well as you. Building your chain is a great way of making a lot of money. larger the chain, more the incentive.</p>
+          <img style={{width: "60%"}} src={chain_img} alt="chain image"/>
         </section>
         <section>
           <h2>How can I create a chain?</h2>
