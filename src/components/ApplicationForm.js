75916301031877e7444b174b8e525b0f91c5bf65
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-31 20:58:47 +0530

    Completed styling for Application Form

--- src/components/ApplicationForm.js
+++ src/components/ApplicationForm.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
+import './ApplicationForm.css'
+
 function ApplicationForm() {
     return (
-        <div>
-            {/* <head> 
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> 
-                <title>Connector Application Form</title> 
-                <style type="text/css"> html{ margin: 0; height: 100%; overflow: hidden; } iframe{ position: absolute; left:0; right:0; bottom:0; top:0; border:0; } </style> 
-            </head>  */}
+        <div className="formBody">
             <body> 
                 <iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://anubhav641267.typeform.com/to/JZCrB8"></iframe> 
                 <script type="text/javascript" src="https://embed.typeform.com/embed.js"></script> 
