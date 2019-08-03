521acd152d24a473e6e0621efb287aebefde51cf
Anubhav311 <anubhavbhambri1@gmail.com>
2019-08-03 05:47:57 +0530

    added incorrect credentials message

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import './Login.css'
+import './Login.css';
+
 function Login() {
+    const [login, setLogin] = useState(false)
+
+    function changeLogin(e) {
+        e.preventDefault()
+        setLogin(true)
+    }
+
     return (
         <div className="login-container">
             <div className="form-container">
+                {login ? alert("Please provide correct username and password") : console.log() }
                 <form className="form">
                     <input placeholder="username" type="text"/>
                     <input placeholder="password" type="password"/>
-                    <input type="submit" className="login-button"/>
+                    <input type="submit" className="login-button" onClick={e => changeLogin(e)}/>
                 </form>
                 <p>Don't have an Account?</p>
                 <p>Please APPLY for creating an account.</p>
