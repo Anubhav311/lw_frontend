25804ba3fcf48ac0312d46b8d412a16658b63ff7
Anubhav311 <anubhavbhambri1@gmail.com>
2019-08-03 05:31:52 +0530

    created login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import './Login.css'
 function Login() {
     return (
-        <div>
-            <form>
-                <input type="text"/>
-                <input type="text"/>
-                <input type="submit"/>
-            </form>
+        <div className="login-container">
+            <div className="form-container">
+                <form className="form">
+                    <input placeholder="username" type="text"/>
+                    <input placeholder="password" type="password"/>
+                    <input type="submit" className="login-button"/>
+                </form>
+                <p>Don't have an Account?</p>
+                <p>Please APPLY for creating an account.</p>
+            </div>
         </div>
     )
 }
