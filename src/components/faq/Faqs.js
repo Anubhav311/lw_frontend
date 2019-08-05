e8f51905f8ce51af62f18afccd129af756f2fb2d
Anubhav311 <anubhavbhambri1@gmail.com>
2019-08-05 09:11:41 +0530

    changed text on the page

--- src/components/faq/Faqs.js
+++ src/components/faq/Faqs.js
@@ -1,52 +1,54 @@
 import React from 'react';
 
-
-
 let faqContent = {
     LinkWork: {
       questionOne: {
         title: "WHAT IS LINKWORK?",
-        content: "LinkWork is worlds first and only platform for which let's you make money without doing any work or spending any time.",
+        content: "LinkWork is worlds first and only platform which let's you make money without doing any work or spending any time.",
       },
       questionTwo: {
         title: "HOW WILL LINKWORK PROVIDE SERVICES TO CLIENT?",
-        content: "LinkWork is connected with world class professional firms. Our network of professional firms will provide the services to client.",
+        content: "LinkWork is connected with world class professional firms. Our network of professional firms will provide the services to clients.",
       },
       questionThree: {
-        title: "WHAT TYPE OF CLIENTS?",
-        content: "GST, Accounts, Starting Business, Company Registration, Income Tax, Labour Law or any other legal or financial service.",
+        title: "WHAT TYPE OF CLIENTS CAN I GIVE TO LINKWORK?",
+        content: "Anyone who needs GST, Accounts, Starting Business, Company Registration, Income Tax, Labour Law or any other legal or financial service.",
+      },
+      questionFour: {
+        title: "DO I HAVE TO PAY ANYTHING?",
+        content: "No. We will never ask you to pay anything. Never.",
+      },
+      questionFive: {
+        title: "LINKWORK IS A SCAM OR FRAUD?",
+        content: "We know LinkWork is too good to be true. But we are real. If you don't believe, just try us and see for yourself.",
       }
     },
     HowMakeMoney : {
       questionOne: {
         title: "HOW CAN I MAKE MONEY",
-        content: "by referring clients and creating a chain of people working under you.",
+        content: "by giving clients and creating a chain of people working under you.",
       },
       questionTwo: {
         title: "HOW MANY HOURS DO I NEED TO WORK?",
-        content: "Zero. You don't have to work at all. But you can work if you want. Just refer a client to us and we will take care of rest.",
+        content: "Zero. You don't have to work at all. But you can work if you want. Just refer a client to us and we will take care of everything else.",
       },
       questionThree: {
-        title: "DO I NEED TO PAY ANYTHING?",
-        content: "NO. We will never ask you to pay anything. Never. Ever.",
+        title: "HOW MUCH MONEY CAN I MAKE?",
+        content: "If you work alone you will make minimum 50,000 by giving at least 1 client. Maximum depends on how many clients you give. If you create your chain of people, there's no limit on the amounnt of money you can make. You can make a lot of money if you create your chain.",
       },
       questionFour: {
-        title: "HOW MUCH MONEY CAN I MAKE?",
-        content: "If you work alone you will make minimum 50,000. If you create your chain of people, there's no limit on the amounnt of money you can make. Maximum depends on your performance.",
-      },
-      questionFive: {
         title: "HOW CAN I PASS THE INTERVIEW",
-        content: "If you have links and work with businessmen, there's a good chance you will be accepted. If you have experience of dealing with people. If your job includes dealing with clients. If you have worked in B2B industry.",
+        content: "There's a good chance that you will pass the interview if you have links and work with businessmen, or if you have experience of dealing with people, or if your job includes dealing with clients, or if you have worked in B2B industry.",
       }
     },
     CreateChain : {
       questionOne: {
         title: "HOW CAN I CREATE A CHAIN?",
-        content: "After you are accepted, you can add more people under you and create a chain. For more details on creating chain go to this LINK.",
+        content: "After you are accepted, you can add more people under you and create a chain. They will apply for an interview and if they are accepted, they will be added under you as part of your chain.",
       },
       questionTwo: {
         title: "WHAT IS THE BENEFIT OF CREATING A CHAIN?",
-        content: "You chain will give Business to LinkWork and we will pay incentive to you and your chain.",
+        content: "You chain will give Business to LinkWork and we will pay incentive to you and your chain. You will make money from your chain.",
       },
       questionThree: {
         title: "HOW MANY PEOPLE CAN I ADD IN MY CHAIN?",
@@ -54,16 +56,15 @@ let faqContent = {
       },
       questionFour: {
         title: "HOW MUCH INCENTIVE WILL I GET FROM MY CHAIN",
-        content: "You will get 20% commission from a direct client. You will earn 10% from people on level 1 of your chain. You will earch 5% from people on level 2 of your chain."
+        content: "You will get 20% commission from a direct client. You will earn 10% from people on level 1 of your chain. You will earn 5% from people on level 2 of your chain."
       }
     },
     MoreQuestions: {
       questionOne: {
         title: "I HAVE MORE QUESTIONS",
-        content: "You can apply and provide your details. I we find your profile suitable we will call you and you can ask all the questions that you have.",
+        content: "You can apply and provide your details. If we find your profile suitable we will call you and you can ask all the questions that you have.",
       }
     }
-  
   }
 
   
